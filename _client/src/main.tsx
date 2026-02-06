3fe87063e05aa0162ece993d283ec586e974ab69
steverova <steverova0594@gmail.com>
2026-02-05 20:17:00 -0600

    addead routes and shadcn layout

--- _client/src/main.tsx
+++ _client/src/main.tsx
@@ -1,14 +1,20 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./app";
+import React, { Suspense } from 'react'
+import { createRoot } from 'react-dom/client'
 import './index.css'
+import { RouterProvider } from 'react-router'
+import { ThemeProvider } from './components/providers/theme-provider'
+import LoadingFallback from './components/ui/loading-fallback'
+import { router } from './routes/router'
 
-
-const rootElement: HTMLElement | null = document.getElementById('root');
-if (!rootElement) throw new Error('Root element not found');
+const rootElement: HTMLElement | null = document.getElementById('root')
+if (!rootElement) throw new Error('Root element not found')
 
 createRoot(rootElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+	<React.StrictMode>
+		<ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+			<Suspense fallback={<LoadingFallback />}>
+				<RouterProvider router={router} />
+			</Suspense>
+		</ThemeProvider>
+	</React.StrictMode>
+)
